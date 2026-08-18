a607e90506c4b52ba0ea29d89c4ab313541fbfc2
kawa-hussein <dekoraserbil@gmail.com>
2026-08-19 02:06:39 +0300

    V6 phase 1: split enterprise CSS for maintainability

--- Education_Platform_V1_Foundation/src/main.tsx
+++ Education_Platform_V1_Foundation/src/main.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import "./v6.css";
+import "./v6-shell.css";
+import "./v6-workspace.css";
+import "./v6-tenant.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><App/></React.StrictMode>
